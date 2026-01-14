b8f9c9e2496cafb279015c45175b704c60c46b30
ZeroBug404 <404.zerobug@gmail.com>
2026-01-15 03:32:05 +1000

    fixes done

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,8 @@ export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     // baseUrl: "http://localhost:5001/api/v1",
-    baseUrl: "https://mfs-server-eosin.vercel.app/api/v1",
+    // baseUrl: "https://mfs-server-eosin.vercel.app/api/v1",
+    baseUrl: "https://mfs-server-zsrv.vercel.app/api/v1",
     prepareHeaders: (headers) => {
       const token = getFromLocalStorage("accessToken");
       if (token) {
